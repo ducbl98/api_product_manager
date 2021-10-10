68a3d2934065496386408498084e3ce19253780a
ducbl98 <ductit1998@gmail.com>
2021-10-10 21:27:44 +0700

    first commit

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        $categories = Category::all();
+        $data = [
+            'status' => '404',
+            'categories' => $categories
+        ];
+        return response()->json($data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $category = new Category();
+        $category->fill($request->all());
+        $category->save();
+        $data = [
+            'status' => 'success',
+            'message' => 'Successful',
+        ];
+        return response()->json($data);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($id)
+    {
+        $category = Category::find($id);
+        $data=[
+            'status'=>'success',
+            'category'=>$category,
+        ];
+        return response()->json($data);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, $id)
+    {
+        $category=Category::find($id);
+        $category->fill($request->all());
+        $category->save();
+        $data = [
+            'status' => 'success',
+            'message' => 'Successful',
+        ];
+        return response()->json($data);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+}
